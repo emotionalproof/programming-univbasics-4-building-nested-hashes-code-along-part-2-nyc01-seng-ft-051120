44eb9f2a206c73cff3f2212f823adc4703ef5129
Chase <chaserabenn@gmail.com>
2020-05-05 17:04:58 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,6 +22,15 @@ def update_status
 
   # Write code that changes the status of both Romeo and Juliet from "alive" to "dead"
   # Code your solution here:
+  epic_tragedy.each do |family_hash|
+    family_hash.each do |person|
+      if person.key == :hero
+        :hero[:status] = "dead"
+      elsif person.key == :heroine
+        :hero[:status] = "dead"        
+      end
+    end
+  end
 
 
 
@@ -56,8 +65,8 @@ def add_characters
    #
    # - Add an additional :additional_characters key to epic_tragedy and assign it an array as its value
    # - Add two hashes to this array, both with a :name key.
-   #   - The first hash should have the :name key set to "Prince Escalus" 
-   #   - The second hash should have the :name key set to "Apothecary" 
+   #   - The first hash should have the :name key set to "Prince Escalus"
+   #   - The second hash should have the :name key set to "Apothecary"
    # Code your solution here:
 
 
